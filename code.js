4030661110a0b38b81b8c9230a09660056f4e30d
Reaper <laerte.mancio@gmail.com>
2023-06-14 15:18:24 -0300

    final-parte1

--- code.js
+++ code.js
@@ -1,16 +1,48 @@
-const itensMenu = document.getElementById('menu')
-const iconeMenu = document.getElementById('icone')  
 
-function exibeMenu() {    
-    
-    iconeMenu.classList.add('sumir-elemento')
-    itensMenu.classList.toggle('exibir-elemento')  
-    
+
+let sucesso = document.getElementById('caixa-sucesso')
+let primeiro = document.getElementById('primeiro-conteudo')
+let erro = document.getElementById('msg-erro')
+
+let botao = document.getElementById('btn').addEventListener("click", subscribe)
+let botao2 = document.getElementById('btn2').addEventListener("click", dismiss)
+let email = document.getElementById('email-digitado')
+let exibeEmail = document.querySelector('.caixa-conteudo-sucesso strong')
+
+
+
+
+
+
+
+const re = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/
+
+function subscribe() {
+    let validaEmail = re.test(email.value)
+    if (validaEmail === true) {
+        sucesso.classList.remove('esconder')
+        primeiro.classList.add('esconder')
+        sucesso.classList.add('sucesso')
+        exibeEmail.innerHTML = email.value        
+        
+    } else {
+        erro.classList.remove('esconder')
+        erro.classList.add('error')
+        email.classList.remove('input-email')
+        email.classList.add('input-email-erro')
+        
+                         
+    }   
+
 }
 
-function fechaMenu() {
-
-    iconeMenu.classList.remove('sumir-elemento')
-    itensMenu.classList.remove('exibir-elemento')
-    
+function dismiss(){
+        sucesso.classList.add('esconder')
+        primeiro.classList.remove('esconder')
+        sucesso.classList.remove('sucesso')
+        erro.classList.add('esconder')
+        erro.classList.remove('error')
+        email.classList.add('input-email')
+        email.classList.remove('input-email-erro')
+        email.value = ''
 }
